7049f7fb4fd1beff6028d17fb2b482367b945c1d
Warblo3829 <qwerty6528@outlook.com>
2024-08-11 05:12:01 +0000

    pls work

--- update.sh
+++ update.sh
@@ -21,25 +21,25 @@ rm -r snd
 mkdir img
 mkdir loc
 mkdir snd
-for f in $(cat _jslist.txt) ; do 
+for f in $(cat jslist.txt) ; do 
   rm "$f"
 done
 cd img/
 wget --convert-links -O index.html http://orteil.dashnet.org/cookieclicker/img/
 grep -v PARENTDIR index.html | grep '\[IMG' | grep -Po 'a href="\K.*?(?=")' | sed 's/\?.*//' > _imglist.txt
-wget -N -i _imglist.txt -B http://orteil.dashnet.org/cookieclicker/img/
+wget -N -i imglist.txt -B http://orteil.dashnet.org/cookieclicker/img/
 cd ../snd/
 wget --convert-links -O index.html http://orteil.dashnet.org/cookieclicker/snd/
 grep -v PARENTDIR index.html | grep '\[SND' | grep -Po 'a href="\K.*?(?=")' | sed 's/\?.*//' > _sndlist.txt
-wget -N -i _sndlist.txt -B http://orteil.dashnet.org/cookieclicker/snd/
+wget -N -i sndlist.txt -B http://orteil.dashnet.org/cookieclicker/snd/
 cd ../loc/
 wget --convert-links -O index.html http://orteil.dashnet.org/cookieclicker/loc/
 grep -v PARENTDIR index.html | grep '\[TXT' | grep -Po 'a href="\K.*?(?=")' | sed 's/\?.*//' > _loclist.txt
-wget -N -i _loclist.txt -B http://orteil.dashnet.org/cookieclicker/loc/
+wget -N -i loclist.txt -B http://orteil.dashnet.org/cookieclicker/loc/
 cd ../
 wget -O index.html http://orteil.dashnet.org/cookieclicker/
 wget -O style.css http://orteil.dashnet.org/cookieclicker/style.css
-wget -N -i _jslist.txt -B http://orteil.dashnet.org/cookieclicker/
+wget -N -i jslist.txt -B http://orteil.dashnet.org/cookieclicker/
 wget -O grab.txt http://orteil.dashnet.org/patreon/grab.php
 cd ..
 mv ss1.jpg Game/img/ss1.jpg
